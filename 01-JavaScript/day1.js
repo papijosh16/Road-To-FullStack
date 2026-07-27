c9ecf6cc2f1d0ce31d4d8bec869bc0c1112287c7
Joshua June Nombre <joshuacatalan35@gmail.com>
2026-07-27 16:03:18 +0800

    Complete Day 1 JavaScript variables and operators

---
+++ 01-JavaScript/day1.js
@@ -0,0 +1,27 @@
+const name = "Joshua June Nombre";
+const age = 25;
+const course = "BSIT";
+const goal = "Become a Full Stack Developer";
+
+console.log("==== MY PROFILE ====");
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Course: ${course}`);
+console.log(`Goal: ${goal}`);
+
+
+const faveLanguage = "JavaScript";
+const faveFood = "Chicken";
+const dreamCompany = "Google";
+
+console.log("");
+console.log("==== EXTRA INFO ====");
+console.log(`Favorite Language: ${faveLanguage}`);
+console.log(`Favorite Food: ${faveFood}`);
+console.log(`Dream Company: ${dreamCompany}`);
+
+
+const currentYear = 2026;
+const birthYear = 2001;
+
+console.log(`Calculated Age: ${currentYear - birthYear}`);
---
+++ 01-JavaScript/gradeCalculator.js
@@ -0,0 +1,19 @@
+const student = "Joshua";
+const math = 95;
+const science = 90;
+const english = 93;
+
+const average = (math + science + english) / 3;
+
+console.log("===== STUDENT REPORT =====");
+console.log(`Student: ${student}`);
+console.log(`Math: ${math}`);
+console.log(`Science: ${science}`);
+console.log(`English: ${english}`);
+console.log(`Average: ${average}`);
+
+if (average >= 90){
+    console.log("Status: Excellent");
+} else {
+    console.log("Status: good");
+}
