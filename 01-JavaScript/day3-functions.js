eb6412f5cc934c416aa9dc37048ffe07eb5468c9
Joshua June Nombre <joshuacatalan35@gmail.com>
2026-07-30 14:48:47 +0800

    Complete Day 3 JavaScript Functions

---
+++ 01-JavaScript/day3-functions.js
@@ -0,0 +1,18 @@
+function sayCourse(course) {
+    return `Course: ${course}`;
+}
+
+console.log(sayCourse("BSIT"));
+
+function introduce(name, age){
+    return `Hi I'm ${name}
+I am ${age} years old`;
+}
+
+console.log(introduce("Joshua",25));
+
+function square(number){
+    return number * number;
+}
+
+console.log(square(5));
