a874fe2a957c66e1d77b9f399e42a98a0d7fed60
Joshua June Nombre <joshuacatalan35@gmail.com>
2026-09-09 11:45:15 +0800

    Complete Day 7 JavaScript Loops

---
+++ 01-JavaScript/day7-loops.js
@@ -0,0 +1,71 @@
+/*const skills = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+
+console.log("====MY SKILLS====");
+
+for (const skill of skills) {
+    console.log(skill);
+}
+
+let number = 1;
+
+while (number <= 5) {
+    console.log(number);
+    number++;
+}
+
+
+for (let i = 1; i <= 10; i++) {
+
+    if (i === 6){
+        break;
+    }
+
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 5; i++) {
+
+    if (i === 3){
+        continue;
+    }
+
+    console.log(i);
+}*/
+
+console.log("Task 1");
+const dreamcompanies = ["Google", "Microsoft", "OpenAI", "Apple", "Amazon"];
+
+console.log("====DREAM COMPANIES====");
+
+for (const company of dreamcompanies) {
+    console.log(company);
+}
+
+console.log("Task 2");
+let number = 1;
+
+while (number <= 5) {
+    console.log(number);
+    number++;
+}
+
+console.log("Task 3");
+for (let i = 1; i <= 10; i++) {
+
+    if (i === 5){
+        continue;
+    }
+
+    console.log(i);
+}
+
+console.log("Task 3");
+for (let i = 1; i <= 10; i++) {
+
+    if (i === 7){
+        break;
+    }
+
+    console.log(i);
+}
